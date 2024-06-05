16da05edf677a6d2f0129f7da98a6e39a9d11efc
Justin Charo <65956091+jcharo1@users.noreply.github.com>
2024-06-05 09:03:05 -0500

    npmignore added

--- react-toastified/components/Toast.js
+++ react-toastified/components/Toast.js
@@ -1,10 +1,4 @@
-import React, {
-  createContext,
-  useContext,
-  useState,
-  useEffect,
-  useRef,
-} from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 const ToastContext = createContext();
 
 function CloseIcon() {
